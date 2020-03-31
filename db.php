16965add69d8a352ab35b4da4f01479428d5dc03
AbWorldMusic <abworldappteam@gmail.com>
2020-03-31 13:08:50 +0530

    Added database

---
+++ db.php
@@ -0,0 +1,9 @@
+<?php
+
+$servername = "localhost";
+$username = "ABWorldUser";
+$password = "0XR0MF*&jCKE";
+$dbname = "abworld";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+?>
