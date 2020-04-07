377f370aeff58395b2b8c34053393bc40fdea88f
AbWorldMusic <abworldappteam@gmail.com>
2020-04-07 23:56:38 +0530

    Lessons pages

--- add_new_slot.php
+++ add_new_slot.php
@@ -6,16 +6,29 @@
         $branchId=$_POST['branchId'];
         $slotName=$_POST['slotName'];
         
-        $sql = "INSERT into slots(branch_id, slot_name)
-                values('$branchId','$slotName')";
-        if (!$conn) {
-          die("Connection failed: " . mysqli_connect_error());
-        }
-        if (mysqli_query($conn, $sql)) {
-            header("Location: edit_branch.php?id=".$branchId);    
+        
+        $query = "SELECT * FROM slots 
+                    WHERE branch_id='$branchId' AND slot_name='$slotName'";
+        $result = mysqli_query($conn,$query);
+        $count =  mysqli_num_rows($result);
+        if($count== 0)
+        {
+        
+            $sql = "INSERT into slots(branch_id, slot_name) values('$branchId','$slotName')";
+            if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+            }
+            if (mysqli_query($conn, $sql)) {
+                header("Location: edit_branch.php?id=".$branchId);    
+            }
+            else{
+                echo "error";
+            }    // row exists. do whatever you would like to do.
         }
         else{
-            echo "error";
+            header("Location: edit_branch.php?id=".$branchId);    
+            
         }
+        
     }
   ?> 
