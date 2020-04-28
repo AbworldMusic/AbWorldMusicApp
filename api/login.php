bca9dcab63967b09321ffed493ea9b577d2ba329
AbWorldMusic <abworldappteam@gmail.com>
2020-04-28 22:25:34 +0530

    Writing first API

--- api/login.php
+++ api/login.php
@@ -7,19 +7,14 @@
     if(!$conn)
         echo "Connection failed";
     $result = mysqli_query($conn,"SELECT * FROM users");
-    $slno = 1;
+    $flag = 1;
     session_start();
     while($row = mysqli_fetch_array($result)){
         if($row['name']==$username || $row['email']==$username){
             if($row['password']==$password){
-                echo "1";
+                $flag = 1;
             }
-            else{
-                echo "0";
-            }
-        }
-        else{
-            echo "0";
         }
     }
+    echo $flag;
 ?>
