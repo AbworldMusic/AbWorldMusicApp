97ada87c622028f537c2d9e2650485f384df6403
AbWorldMusic <abworldappteam@gmail.com>
2020-05-01 22:05:46 +0530

    Get students api

--- api/get_students_for_class.php
+++ api/get_students_for_class.php
@@ -4,6 +4,7 @@
     $classNames= $_POST['classes'];
     $clasNames = (explode(",",$classNames));
     $branch = $_POST['branch'];
+    $date = $_POST['date'];
 
     if(!$conn)
         echo "Connection failed";
@@ -17,6 +18,16 @@
                 $record->id = $row['id'];
                 $record->name = $row['name'];
 
+                $attendance = mysqli_query($conn,"SELECT * FROM attendance WHERE student_id='$record->id'
+                                            AND date='$date' LIMIT 1");
+                if (mysqli_num_rows($attendance)==0){
+                    $record->attendance = "Absent";
+                }
+                else{
+                    while($row1 = mysqli_fetch_array($attendance)){
+                        $record->attendance = $row1['value'];
+                    }
+                }
                 array_push($entries, $record);   
             }
         }    
---
+++ api/mark_attendance.php
@@ -0,0 +1,44 @@
+<?php 
+    include_once('../db.php');
+
+    $studentId = $_GET['id'];
+    $value = "Present";
+    $date = "01/05/2020";
+    $day = "Firday";
+
+
+    if(!$conn)
+        echo "Connection failed";
+    
+    $result = mysqli_query($conn,"SELECT * FROM attendance WHERE student_id='$studentId' AND date='$date'");
+
+    if (mysqli_num_rows($result)==0){
+        $sql = "INSERT into attendance(student_id, date, value, day) 
+                    values('$studentId','$date','$value','$day')";
+        if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+        }
+        if (mysqli_query($conn, $sql)) {
+            echo "Marked ".$value;
+
+        }
+    }
+    else{
+        while($row = mysqli_fetch_array($result)){
+            $value = $row['value'];
+            if($value=="Present"){
+                $value = "Absent";
+            }
+            else{
+                $value = "Present";
+            }
+            $sql = "UPDATE attendance set value='$value' WHERE student_id='$studentId' AND date='$date'";
+            if (mysqli_query($conn, $sql)) {
+                echo "Marked ".$value;
+    
+            }
+        }
+        
+    }
+
+?>
