6ce964fad150b30aa4c28c27d3d4e2bc380416ae
AbWorldMusic <abworldappteam@gmail.com>
2020-05-04 20:51:43 +0530

    login api fix

--- api/login.php
+++ api/login.php
@@ -9,22 +9,19 @@
     $result = mysqli_query($conn,"SELECT * FROM users");
     
     $entries = array();
+    $flag = 0;
     while($row = mysqli_fetch_array($result)){
         $record =  new \stdClass();
                 
         if($row['name']==$username || $row['email']==$username){
             if($row['password']==$password){
                 $record->id = $row['id'];
-                $record->flag = 1;
+        
+                $flag = 1;
                 $record->role = $row['role'];
             }
-            else{
-                $record->flag = 0;
-            }
         }
-        else{
-            $record->flag = 0;
-        }
+        $record->flag = $flag;
         array_push($entries, $record);   
     }
     echo json_encode($entries);
