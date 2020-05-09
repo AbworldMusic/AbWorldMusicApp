46ca390dad710a828689c19040e1453cae418721
AbWorldMusic <abworldappteam@gmail.com>
2020-05-09 16:13:49 +0530

    Upload image api

---
+++ api/upload_image.php
@@ -0,0 +1,23 @@
+<?php
+    
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        include_once('../db.php');
+        
+        $image = $_FILES['image']['name'];
+        $target = "images/".basename($image);
+
+
+
+        echo $image;
+
+        if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
+            $msg = "Image uploaded successfully";
+        }else{
+            $msg = "Failed to upload image";
+        }
+        if (!$conn) {
+          die("Connection failed: " . mysqli_connect_error());
+        }
+        
+    }
+  ?> 
