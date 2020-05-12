56f0fdbb2a4dec5f0e00cf1627cd7aacf1aaaecc
AbWorldMusic <abworldappteam@gmail.com>
2020-05-12 07:58:45 +0530

    API for current lessons

--- api/mark_promotion.php
+++ api/mark_promotion.php
@@ -12,7 +12,7 @@
     
     
 
-    $result = mysqli_query($conn,"SELECT * FROM Students WHERE student_id='$studentId'");
+    $result = mysqli_query($conn,"SELECT * FROM Students WHERE id='$studentId'");
 
     
     while($row = mysqli_fetch_array($result)){
