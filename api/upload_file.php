6811cb80d30baf9edab7e9c7217e3cb7e3a16cdb
AbWorldMusic <abworldappteam@gmail.com>
2020-05-13 19:44:51 +0530

    API for current lessons

--- api/upload_file.php
+++ api/upload_file.php
@@ -5,16 +5,27 @@
         include_once('../db.php');
         
         $image = $_FILES['image']['name'];
+        $title = $_POST['title'];
+        $date = $_POST['date'];
+        $posted_by = $_POST['posted_by'];
         $target = "images/".basename($image);
 
 
 
-        echo $image;
+        $sql = "INSERT into files(title, date, posted_by, filename)
+                values('$title','$date','$posted_by','$image')";
+
 
         if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
             $msg = "Image uploaded successfully";
         }else{
             $msg = "Failed to upload image";
+        }
+        if (mysqli_query($conn, $sql)) {
+            echo "Uploaded";
+        }
+        else{
+            echo "Error: " . $sql . "<br>" . $conn->error;
         }
         if (!$conn) {
           die("Connection failed: " . mysqli_connect_error());
