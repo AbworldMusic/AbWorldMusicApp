40c2497c98c41d5eabb2011ae491aa9493e2e267
AbWorldMusic <abworldappteam@gmail.com>
2020-06-09 15:42:53 +0530

    Added world of music pages

---
+++ WorldOfMusic/index.php
@@ -0,0 +1,14 @@
+<?php 
+include_once('base.php');
+?>
+<body>
+        <div class='header d-flex justify-content-center bg-light'>
+            <div class="my-4 mx-2 text-center">
+                <h1>World of Music</h1>
+                <h5>Pick what you want to learn today!</h5>
+            </div>
+        </div>
+    <div class='container d-flex justify-content-center my-4'>
+        <a href='enrollment.php' class='btn btn-primary'>Start Enrollment</a>
+    </div>
+</body>
